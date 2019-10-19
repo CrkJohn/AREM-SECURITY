5d2d09ff8edafb78f9711e674c77d408db0badad
CrkJohn <JhonhRge@gmail.com>
2019-10-18 21:25:50 -0400

    Adding details

---
+++ Security/src/main/java/edu/eci/arem/service/controller/MathController.java
@@ -0,0 +1,36 @@
+package edu.eci.arem.service.controller;
+
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping(value="/math")
+public class MathController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    Environment env;
+
+    @RequestMapping(value = "/data/server", method = RequestMethod.GET)
+    public String getServerData() {
+        System.out.println("Got inside server data method");
+        try {
+            String Endpoint = env.getProperty("endpoint.serverData");
+            System.out.println("MS Endpoint name : [" + Endpoint + "]");
+
+            return restTemplate.getForObject(new URI(Endpoint), String.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "Exception occurred.. so, returning default data";
+    }
+}
--- calc/src/main/java/edu/eci/calc/calc/controller/OperationController.java
+++ calc/src/main/java/edu/eci/calc/calc/controller/OperationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 //(@CrossOrigin(origins = "https://localhost:8443/")
-@RequestMapping(value = "v1/operation")
+    @RequestMapping(value = "v1/operation")
 public class OperationController {
 
     @Autowired
